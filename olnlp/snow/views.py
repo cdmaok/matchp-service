0ae1675ff7b477b282444a0f1daf5d73477ab06f
cdmaok <cdmaok@126.com>
2016-04-22 21:20:13 +0800

    finish snownlp

--- olnlp/snow/views.py
+++ olnlp/snow/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import snownlp
+import json
+from snownlp import SnowNLP
 # Create your views here.
 
 def index(request):
-	text = request.GET['text']
-	if text.strip() == '':
-		return HttpResponse('empty text')
+	message = {}
+	if not request.GET.has_key('text'):
+		message['Code'] = '400'
+		message['Message'] = 'invalid request'
 	else:
-		return HttpResponse("Hello----world")
+		text = request.GET['text']
+		if text.strip() == '':
+			message['Code'] = '406'
+			message['Message'] = 'empty text'
+		else:
+			s = SnowNLP(text)
+			score =  s.sentiments
+			print score
+			message['Code'] = '200'
+			message['Message'] = score
+
+	return HttpResponse(json.dumps(message))
