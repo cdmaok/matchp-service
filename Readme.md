31e7590ecf97b0e03aeb2e8b740eb87919759f0c
cdmaok <cdmaok@126.com>
2016-04-25 18:03:15 +0800

    add post method

--- Readme.md
+++ Readme.md
@@ -1,2 +1,18 @@
+## Introduction
+this program is to wrap snownlp with django
+
+
 ## usage
 pip -r requirements.txt
+cd olnlp
+python manage.py runserver 0.0.0.0:8000
+
+##API
+GET -- http://ip:port/snow/?text=xxxx
+POST -- http://ip:port/snow/
+
+##Notice
+For GET method, there is so much limitation about the text such as symbols.
+We can simplely deactive csrf.
+Otherwise we need to use other framework.
+
--- olnlp/snow/views.py
+++ olnlp/snow/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import json
 from snownlp import SnowNLP
 # Create your views here.
 
+@csrf_exempt
 def index(request):
 	message = {}
-	if not request.GET.has_key('text'):
+	print request.method
+	if request.method == 'GET':
+		message = construct_message(request.GET,'text')
+	elif request.method == 'POST':
+		message = construct_message(json.loads(request.body),'text')
+	else:
+		print 'invalid request'
+	return HttpResponse(json.dumps(message))
+
+
+def construct_message(parameter,key):
+	message = {}
+	print type(parameter)
+	if not parameter.has_key(key):
 		message['Code'] = '400'
 		message['Message'] = 'invalid request'
 	else:
-		text = request.GET['text']
+		text = parameter['text']
+		print '======================'
 		if text.strip() == '':
 			message['Code'] = '406'
 			message['Message'] = 'empty text'
@@ -20,5 +36,5 @@ def index(request):
 			print score
 			message['Code'] = '200'
 			message['Message'] = score
+	return message
 
-	return HttpResponse(json.dumps(message))
